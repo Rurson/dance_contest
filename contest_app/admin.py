80401cabc613295aa7305db93afba3c8aef7cc9e
Juliusz Lewalski <juliusz.lewalski@gmail.com>
2019-11-28 04:11:10 +0100

    feat(membership): Added Memberships for Contests

--- contest_app/admin.py
+++ contest_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from contest_app.models import Contest, Membership
+
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 3
+
+
+class ContestAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline]
+
+
+admin.site.register(Contest, ContestAdmin)
+admin.site.register(Membership)
--- contest_app/models.py
+++ contest_app/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Contest(models.Model):
+    title = models.CharField(max_length=128)
+
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    members = models.ManyToManyField(User, through='Membership')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['start_date']
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    type = models.CharField(max_length=25, choices=[('C', 'Contender'), ('J', 'Jury')])
+
+    def __str__(self):
+        return str(self.user)
 
