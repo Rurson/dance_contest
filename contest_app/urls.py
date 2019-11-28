7a7612ba41ca64003784fe2b4649ffe0e90970fd
Juliusz Lewalski <juliusz.lewalski@gmail.com>
2019-11-28 13:34:07 +0100

    feat(contest_app): Added url in index for contest details

--- contest_app/urls.py
+++ contest_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from contest_app import views
 
-app_name='contest_app'
+app_name='contest'
 
 urlpatterns = [
     path('',views.IndexView.as_view(), name='index'),
-    path('<pk>/details', views.DetailsView.as_view(), name='details')
+    path('details/<pk>', views.DetailsView.as_view(), name='details')
 ]
